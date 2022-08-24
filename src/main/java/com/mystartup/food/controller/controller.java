f0e9cf9a23cbeab096d1e701eb132f08e3ca6d97
KlevCode <appris@gmx.net>
2022-08-24 21:28:05 +0200

    eins

--- src/main/java/com/mystartup/food/controller/controller.java
+++ src/main/java/com/mystartup/food/controller/controller.java
@@ -1,5 +1,6 @@
 package com.mystartup.food.controller;
 
+import com.mystartup.food.model.Food;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,10 @@ public class controller {
   @GetMapping("/home")
   public String getHome(Model model) {
     model.addAttribute("message", "Hello there, this is a message from the controller");
+
+    Food[] foods = {new Food("Pizza", 8.00), new Food("Doner", 4.00), new Food("Cola", 1.50)};
+
+    model.addAttribute("foods", foods);
     return "home";
   }
 }
